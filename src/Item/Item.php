0b4faf53913b62118bd4eb236e22b83cbababd79
Daniel Waters <me@dlwaters.co.uk>
2016-10-26 20:20:08 +0100

    item itself doesn't have quantity

--- src/Item/Item.php
+++ src/Item/Item.php
@@ -5,6 +5,6 @@ namespace dlwatersuk\Sagepay\Item;
 
 interface Item
 {
-    public function __construct(Array $item=[], $quantity=1);
+    public function __construct(Array $item=[]);
     public function __get($var);
 }
