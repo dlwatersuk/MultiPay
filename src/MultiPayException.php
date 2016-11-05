861eefc275061f27528b5770f9f9fffd669bcb72
Daniel Waters <me@dlwaters.co.uk>
2016-11-05 01:06:38 +0000

    fix Exception extension

--- src/MultiPayException.php
+++ src/MultiPayException.php
@@ -3,7 +3,7 @@
 namespace dlwatersuk\MultiPay;
 
 
-class MultiPayException extends Exception
+class MultiPayException extends \Exception
 {
     public function __construct($message, $code = 0, Exception $previous = null) {
         parent::__construct($message,$code,$previous);
